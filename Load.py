31b0c322ae2ff09167067e167686b2b10d607789
Saschaszaruga <132253318+Saschaszaruga@users.noreply.github.com>
2023-05-05 12:53:21 +0200

    Add files via upload

---
+++ Load.py
@@ -0,0 +1,89 @@
+import sys
+import pygame
+
+
+# Define some colors
+WHITE = (255, 255, 255)
+BLUE = (240, 248, 255)
+TRANSPARENT_BLUE = (64, 0, 255, 42)
+
+# Set the dimensions of the screen
+SCREEN_WIDTH = 850
+SCREEN_HEIGHT = 478
+
+# Initialize Pygame
+pygame.init()
+
+# Set the size of the screen
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+# Set the title of the window
+pygame.display.set_caption('SPACE INVADERS')
+
+# Load the menu background image
+background_image1 = pygame.image.load("background1.png").convert()
+
+# Create a font object
+font = pygame.font.Font('Starwars.TTF', 36)
+
+# Create the start button
+start_button = font.render('Start Game', True, WHITE)
+start_rect = start_button.get_rect()
+start_rect.centerx = SCREEN_WIDTH // 2
+start_rect.centery = SCREEN_HEIGHT // 2 - 100
+
+# Create the quit button
+quit_button = font.render('Quit Game', True, WHITE)
+quit_rect = quit_button.get_rect()
+quit_rect.centerx = SCREEN_WIDTH // 2
+quit_rect.centery = SCREEN_HEIGHT // 2
+
+
+
+
+# Create semi-transparent surfaces for button backgrounds
+start_button_bg = pygame.Surface((start_button.get_width() + 20, start_button.get_height() + 10), pygame.SRCALPHA)
+start_button_bg.fill(TRANSPARENT_BLUE)
+start_button_bg_rect = start_button_bg.get_rect(center=start_rect.center)
+
+quit_button_bg = pygame.Surface((quit_button.get_width() + 20, quit_button.get_height() + 10), pygame.SRCALPHA)
+quit_button_bg.fill(TRANSPARENT_BLUE)
+quit_button_bg_rect = quit_button_bg.get_rect(center=quit_rect.center)
+
+# Create a variable to keep track of whether the game is running or not
+game_running = True
+
+while game_running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Check if the start button was clicked
+            if start_rect.collidepoint(event.pos):
+                # Start the game here
+                if __name__ == '__main__':
+                    import main
+                    # Check if the quit button was clicked
+            if quit_rect.collidepoint(event.pos):
+                # Quit the game here
+                game_running = False
+
+
+    # Draw objects here
+
+    # Blit the buttons and backgrounds on the screen
+    screen.blit(background_image1, [0, 0])
+    screen.blit(start_button_bg, start_button_bg_rect)
+    screen.blit(start_button, start_rect)
+    screen.blit(quit_button_bg, quit_button_bg_rect)
+    screen.blit(quit_button, quit_rect)
+
+    # Update the screen
+    pygame.display.update()
+
+# Quit the game
+pygame.quit()
+sys.exit()
